e13c56a2e1c1ab7aa65fb5815f7c524aa6763ef1
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-24 14:45:22 -0300

    Creating devise User

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,7 +311,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.navigational_formats = []
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    email {  Faker::Internet.email }
+    password { '123456' }
+  end
+end
+
